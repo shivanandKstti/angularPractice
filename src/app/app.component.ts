14d19a3c23adc867ae638f06b84755bd056b98ea
shivusoumya <shivadatacamp@gmail.com>
2021-03-26 08:55:02 +0530

    angular practice

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -11,5 +11,19 @@ export class AppComponent {
   //   title: 'title',
   //   isFavorite: true
   // }
-  courses = [];
+  // courses = [];
+  // viewMode = 'map';
+  courses = [
+    {id: 1, name: 'course1'},
+    {id: 2, name: 'course2'},
+    {id: 3, name: 'course3'},
+  ];
+
+  onadd(){
+    this.courses.push({id: 4, name: 'course4'});
+  }
+  onRemove(course){
+    let index = this.courses.indexOf(course);
+    this.courses.splice(index, 1);
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,3 +1,4 @@
+import { SignupFormComponent } from './signup-form/signup-form.component';
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
@@ -6,16 +7,22 @@ import { CoursesComponent } from './courses.component';
 import { CourseComponent } from './course/course.component';
 import { CoursesService } from './courses.service';
 import { FavoriteComponent } from './favorite/favorite.component';
+import { ContactFormComponent } from './contact-form/contact-form.component';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
     AppComponent,
     CoursesComponent,
     CourseComponent,
-    FavoriteComponent
+    FavoriteComponent,
+    ContactFormComponent,
+    SignupFormComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule,
+    ReactiveFormsModule
   ],
   providers: [
     CoursesService
